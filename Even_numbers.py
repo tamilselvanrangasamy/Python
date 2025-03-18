b4ec07a403b87336ae9a78d08788596c466e5521
Tamilselvan Rangasamy <129967226+tamilselvanrangasamy@users.noreply.github.com>
2025-03-18 16:56:21 +0530

    Add files via upload

---
+++ Even_numbers.py
@@ -0,0 +1,7 @@
+even_numbers=[] #Creating an Empty list
+
+for element in range(201):
+    if( element % 2 == 0): 
+        even_numbers.append(element) #Adding even numbers in the list
+
+print(even_numbers)
