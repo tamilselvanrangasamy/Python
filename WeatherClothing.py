8cc005a85ecf8d46f2adf1fea43a7bd81fbb2d32
Tamilselvan Rangasamy <129967226+tamilselvanrangasamy@users.noreply.github.com>
2025-03-25 15:59:40 +0530

    Add files via upload

---
+++ WeatherClothing.py
@@ -0,0 +1,7 @@
+temperature=float(input("Enter temperature in celsius: "))
+if temperature < 10:
+    print("It's cold! Wear a warm coat and scarf.")
+elif temperature <= 25:
+    print("It's mild! A light jacket will do.")
+else:
+    print("It's hot! T-shirt would be perfect.")
